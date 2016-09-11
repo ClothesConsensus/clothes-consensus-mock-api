73d3f317a90605d8ce4aab44ebafafb1e8b68841
Ryan Newton <rnewton@hearsaycorp.com>
2016-09-11 11:12:47 -0700

    Add looks to /user/:id/ endpoint

--- api.rb
+++ api.rb
@@ -83,9 +83,13 @@ post '/users/' do
   user.to_json
 end
 
-get '/users/:id' do
+get '/users/:id/' do
   content_type :json
-  User.find(params['id']).to_json
+  user = User.find(params['id']).to_json({
+    include: [{looks: {
+      methods: :vote_results
+    }}]
+  })
 end
 
 delete '/users/:id' do
